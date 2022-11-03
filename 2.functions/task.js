a228d907baf71f65a72c8d0f5f963182945386d0
Pavel Cherepanov <cherepanov.pavel@list.ru>
2022-11-03 18:50:44 +0500

    modified:   2.functions/task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,66 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
 
   // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+  console.log(sum)
+
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
   // Ваш код
-
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
+
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
   // Ваш кода
+  
   // for ...
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
+  let min = Math.abs(arr[0]);
+  let max = Math.abs(arr[0]);
+  let difference = 0;
   // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (Math.abs(arr[i]) > max) {
+      max = Math.abs(arr[i]);
+    } else if (Math.abs(arr[i]) < min) {
+      min = Math.abs(arr[i]);
+    }
+    difference = max - min;
+  }
+  return difference;
 }
