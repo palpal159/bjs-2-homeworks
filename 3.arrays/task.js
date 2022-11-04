e1ec9a42c931831e55d52109b53bc61599bfb6ca
Pavel Cherepanov <cherepanov.pavel@list.ru>
2022-11-04 11:55:59 +0500

    modified:   3.arrays/task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,14 +2,24 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
-
-  return result; // boolean
+  if (arr1.length != arr2.length) {
+    result = false;
+  } 
+  for(var i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      result = false; 
+    } else if (arr1.length === arr2.length && arr1[i] === arr2[i]) {
+      result = true;
+    }
+  return result; 
+  }
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = [];
 
   // Ваш код
+  resultArr = arr.filter(num => num > 0).filter(num => num % 3 == 0).map(num => num * 10);
 
   return resultArr; // array
 }
