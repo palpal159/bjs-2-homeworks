d32a5d02e8e8c3b7d60bc3279dc59ea0578851b4
Pavel Cherepanov <cherepanov.pavel@list.ru>
2022-11-09 23:15:39 +0500

    modified:   4.objects/task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,42 @@
 function Student(name, gender, age) {
     // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+
+let firstStudent = new Student("Anton", "male", 23);
+let seconStudent = new Student("Ivan", "male", 28);
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  this.subject = subjectName;
 }
 
 // ваш код для остальных методов
+
+Student.prototype.addMark = function (mark) {
+  if ( this.marks === undefined) {
+  this.marks = [mark]; 
+  } else {
+  this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  this.marks = [...mark];
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
