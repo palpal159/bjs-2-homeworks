6df26b3fe88c1158ad30e0f1a3c6c2e58176db8e
Pavel Cherepanov <cherepanov.pavel@list.ru>
2022-12-07 16:53:49 +0500

    modified:   8.decorators/task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,42 @@
 function cachingDecoratorNew(func) {
   // Ваш код
+    let cache = [];
+  
+    function wrapper(...args) {
+      const hash = args.join(','); 
+      let objectInCache = cache.find((item) => item.hash === hash);
+      if (objectInCache) { 
+          console.log("Из кэша: " + objectInCache.value); 
+          return "Из кэша: " + objectInCache.value;
+      }
+  
+      let result = func(...args);
+      let value = result;
+      cache.push({hash, value});
+      if (cache.length > 5) { 
+        cache.shift() 
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;  
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
+function debounceDecoratorNew(func, delay) {
   // Ваш код
+  let timeoutId = null;
+  function wrapper(...args) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      wrapper.allCount++
+    }
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count++;
+    }, delay);
+  }
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  return wrapper;
 }
